bcb0c33659c71aca1c7a55aa893db48248375157
rubin-khadka <rubin.robotic@gmail.com>
2026-06-02 18:06:16 +0200

    add necessary file

---
+++ Src/main.c
@@ -0,0 +1,13 @@
+/*
+ * @file           : main.c
+ * @author         : Rubin Khadka
+ * @brief          : Main program body
+ */
+
+#include <stdint.h>
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
